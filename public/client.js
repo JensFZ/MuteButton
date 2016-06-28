70c67699da84cccd4b2da064c02f6b9f6198438d
Jens Fischer <JensFZ@MacBook-Air.fritz.box>
2016-06-28 23:46:09 +0200

    First commit

---
+++ public/client.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+  var $usernameWeiter = $('.usernameWeiter');
+  var $usernameInput = $('.usernameInput');
+  var $inputMessage = $('.inputMessage');
+  var $loginPage = $('.login.page');
+  var $chatPage = $('.chat.page');
+  var socket = io.connect();
+  var noSleep = new NoSleep();
+
+  function enableNoSleep() {
+    noSleep.enable();
+    document.removeEventListener('touchstart', enableNoSleep, false);
+  }
+  
+  socket.on('chat', function (data) {
+    var zeit = new Date(data.zeit);
+    $('#content').append(
+        $('<li></li>').append(
+            // Uhrzeit
+            $('<span>').text('[' +
+                (zeit.getHours() < 10 ? '0' + zeit.getHours() : zeit.getHours())
+                + ':' +
+                (zeit.getMinutes() < 10 ? '0' + zeit.getMinutes() : zeit.getMinutes())
+                + '] '
+            ),
+            // Name
+            $('<b>').text(typeof(data.name) != 'undefined' ? data.name + ': ' : ''),
+            // Text
+            $('<span>').text(data.text))
+    );
+    // nach unten scrollen
+    $('body').scrollTop($('body')[0].scrollHeight);
+  });
+
+  function senden(){
+	  // Eingabefelder auslesen
+	  var name = $('#name').val();
+	  var text = $('#text').val();
+	  // Socket senden
+    socket.emit('chat', { name: name, text: text });
+    // Text-Eingabe leeren
+    $('#text').val('');
+  }
+  // bei einem Klick
+  $('#senden').click(senden);
+  // oder mit der Enter-Taste
+  $('#text').keypress(function (e) {
+  	if (e.which == 13) {
+  		senden();
+  	}
+  });
+
+
+  $usernameWeiter.on('click', function() {
+    setUsername();
+  });
+
+  // Username setzen
+  function setUsername () {
+    username = cleanInput($usernameInput.val().trim());
+
+    // If the username is valid
+    if (username) {
+      $loginPage.fadeOut();
+      $chatPage.show();
+      $loginPage.off('click');
+      $currentInput = $inputMessage.focus();
+
+      // Tell the server your username
+      socket.emit('add user', username);
+    }
+  }
+
+  // Prevents input from having injected markup
+  function cleanInput (input) {
+    return $('<div/>').text(input).text();
+  }
+
+});
---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+
+// Setup
+var express  = require('express');
+var app      = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var morgan   = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var conf = require('./config.json');
+
+
+// conf
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+//app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// wenn der Pfad / aufgerufen wird
+app.get('/', function (req, res) {
+	// so wird die Datei index.html ausgegeben
+	res.sendfile(__dirname + '/public/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+	var addedUser = false;
+
+	// der Client ist verbunden
+	socket.on('add user', function(username) {
+		if (addedUser) return;
+
+		addedUser = true;
+		socket.username = username;
+		console.log('user ' + username + ' connected');
+	});
+
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
+	// wenn ein Benutzer einen Text senden
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+    console.log('Name: ' + data.name + " Text: " + data.text);
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
+	});
+});
+
+// listen (start app with node server.js) ======================================
+server.listen(conf.port);
+console.log("App listening on port " + conf.port);
