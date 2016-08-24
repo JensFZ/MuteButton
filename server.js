1c95598e0f4ecb0341d16347d08c7590787934b5
Jens Fischer <JensFZ@gmail.com>
2016-08-24 22:51:26 +0200

    Code style issues fixed

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 // server.js
 
 // Setup
-var express  = require('express');
+var express  = require("express");
 var app      = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-var morgan   = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var conf = require('./config.json');
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var morgan   = require("morgan");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var conf = require("./config.json");
 
 
 // conf
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-//app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(express.static(__dirname + "/public"));                 // set the static files location /public/img will be /img for users
+//app.use("/bower_components",  express.static(__dirname + "/bower_components"));
+app.use(morgan("dev"));                                         // log every request to the console
+app.use(bodyParser.urlencoded({"extended":"true"}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
 // wenn der Pfad / aufgerufen wird
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
 	// so wird die Datei index.html ausgegeben
-	res.sendfile(__dirname + '/public/index.html');
+	res.sendfile(__dirname + "/public/index.html");
 });
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
 	var addedUser = false;
 
 	// der Client ist verbunden
-	socket.on('add user', function(username) {
+	socket.on("add user", function(username) {
 		if (addedUser) {
 			return;
 		}
