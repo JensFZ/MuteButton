38718333a8ea2533ddff8fe0ddc90fc5be8fe8dc
Jens Fischer <JensFZ@gmx.de>
2016-08-24 22:14:26 +0200

    Code style fixes

--- public/client.js
+++ public/client.js
@@ -9,22 +9,22 @@ $(document).ready(function(){
 
   function enableNoSleep() {
     noSleep.enable();
-    document.removeEventListener('touchstart', enableNoSleep, false);
+    document.removeEventListener("touchstart", enableNoSleep, false);
   }
 
-  socket.on('chat', function (data) {
+  socket.on("chat", function (data) {
     var zeit = new Date(data.zeit);
     $("#content").append(
         $("<li></li>").append(
             // Uhrzeit
             $("<span>").text("[" +
-                (zeit.getHours() < 10 ? '0' + zeit.getHours() : zeit.getHours())
+                (zeit.getHours() < 10 ? "0" + zeit.getHours() : zeit.getHours())
                 + ":" +
-                (zeit.getMinutes() < 10 ? '0' + zeit.getMinutes() : zeit.getMinutes())
+                (zeit.getMinutes() < 10 ? "0" + zeit.getMinutes() : zeit.getMinutes())
                 + "] "
             ),
             // Name
-            $("<b>").text(typeof(data.name) != 'undefined' ? data.name + ': ' : ''),
+            $("<b>").text(typeof(data.name) != "undefined" ? data.name + ': ' : ''),
             // Text
             $("<span>").text(data.text))
     );
@@ -34,10 +34,12 @@ $(document).ready(function(){
 
   function senden(){
 	  // Eingabefelder auslesen
-	  var name = $("#name").val();
-	  var text = $("#text").val();
+    var name = $("#name").val();
+    var text = $("#text").val();
+
 	  // Socket senden
     socket.emit("chat", { name: name, text: text });
+
     // Text-Eingabe leeren
     $("#text").val("");
   }
@@ -46,7 +48,7 @@ $(document).ready(function(){
   // oder mit der Enter-Taste
   $("#text").keypress(function (e) {
   	if (e.which == 13) {
-  		senden();
+      senden();
   	}
   });
 
