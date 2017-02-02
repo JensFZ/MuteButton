e9da7ab29eb8d4c469d6281d5fab1ddd1e516420
Jens Fischer <JensFZ@gmail.com>
2017-02-03 00:36:10 +0100

    some code cleanup

--- public/client.js
+++ public/client.js
@@ -1,99 +1,96 @@
 $(document).ready(function(){
-
-  var dialog = document.querySelector('dialog');
-  var showModalButton = document.querySelector('.show-modal');
-  if (! dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-  }
-  showModalButton.addEventListener('click', function() {
-    dialog.showModal();
-  });
-  dialog.querySelector('.close').addEventListener('click', function() {
-    setUsername();
-    dialog.close();
-  });
-
-
-  var $usernameWeiter = $(".usernameWeiter");
-  var $inputMessage   = $(".inputMessage");
-  var $loginPage      = $(".login.page");
-  var $chatPage       = $(".chat.page");
-  var socket          = io.connect();
-  var noSleep         = new NoSleep();
-
-  function enableNoSleep() {
-    noSleep.enable();
-    document.removeEventListener("touchstart", enableNoSleep, false);
-  }
-
-  socket.on("chat", function (data) {
-    var zeit = new Date(data.zeit);
-    $("#content").append(
-      $("<li></li>").append(
-        // Uhrzeit
-        $("<span>").text("[" +
-        (zeit.getHours() < 10 ? "0" + zeit.getHours() : zeit.getHours())
-        + ":" +
-        (zeit.getMinutes() < 10 ? "0" + zeit.getMinutes() : zeit.getMinutes())
-        + "] "
-      ),
-      // Name
-      $("<b>").text(typeof data.name !== "undefined" ? data.name + ": " : ""),
-      // Text
-      $("<span>").text(data.text))
-    );
-    // nach unten scrollen
-    $("body").scrollTop($("body")[0].scrollHeight);
-  });
-
-  function senden(){
-    // Eingabefelder auslesen
-    var name = $("#name").val();
-    var text = $("#text").val();
-
-    // Socket senden
-    socket.emit("chat", { name: name, text: text });
-
-    // Text-Eingabe leeren
-    $("#text").val("");
-  }
-  // bei einem Klick
-  $("#senden").click(senden);
-  // oder mit der Enter-Taste
-  $("#text").keypress(function (e) {
-    if (e.which === 13) {
-      senden();
+    var dialog = document.querySelector('dialog');
+    var showModalButton = document.querySelector('.show-modal');
+    if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
     }
-  });
+    showModalButton.addEventListener('click', function() {
+        dialog.showModal();
+    });
+    dialog.querySelector('.close').addEventListener('click', function() {
+        setUsername();
+        dialog.close();
+    });
 
-  // Prevents input from having injected markup
-  function cleanInput(input) {
-    return $("<div/>").text(input).text();
-  }
+    var $usernameWeiter = $(".usernameWeiter");
+    var $inputMessage   = $(".inputMessage");
+    var $loginPage      = $(".login.page");
+    var $chatPage       = $(".chat.page");
+    var socket          = io.connect();
+    var noSleep         = new NoSleep();
 
-  // Username setzen
-  function setUsername() {
-    console.log('hier');
-
-    username = cleanInput(dialog.querySelector('.Username').value.trim()); //cleanInput($usernameInput.value().trim());
-    console.log(username);
-
-    // If the username is valid
-    if (username) {
-      $loginPage.fadeOut();
-      $chatPage.show();
-      $loginPage.off("click");
-      $currentInput = $inputMessage.focus();
-
-      // Tell the server your username
-      socket.emit("add user", username);
+    function enableNoSleep() {
+        noSleep.enable();
+        document.removeEventListener("touchstart", enableNoSleep, false);
     }
-  }
 
-  $usernameWeiter.on("click", function() {
-    setUsername();
-  });
+    socket.on("chat", function (data) {
+        var zeit = new Date(data.zeit);
+        $("#content").append(
+            $("<li></li>").append(
+            // Uhrzeit
+            $("<span>").text("[" +
+                (zeit.getHours() < 10 ? "0" + zeit.getHours() : zeit.getHours())
+                + ":" +
+                (zeit.getMinutes() < 10 ? "0" + zeit.getMinutes() : zeit.getMinutes())
+                + "] "
+            ),
+            // Name
+            $("<b>").text(typeof data.name !== "undefined" ? data.name + ": " : ""),
+            // Text
+            $("<span>").text(data.text))
+        );
+        // nach unten scrollen
+        $("body").scrollTop($("body")[0].scrollHeight);
+    });
 
-  document.addEventListener("touchstart", enableNoSleep, false);
+    function senden(){
+        // Eingabefelder auslesen
+        var name = $("#name").val();
+        var text = $("#text").val();
 
+        // Socket senden
+        socket.emit("chat", { name: name, text: text });
+
+        // Text-Eingabe leeren
+        $("#text").val("");
+    }
+    // bei einem Klick
+    $("#senden").click(senden);
+    // oder mit der Enter-Taste
+    $("#text").keypress(function (e) {
+        if (e.which === 13) {
+            senden();
+        }
+    });
+
+    // Prevents input from having injected markup
+    function cleanInput(input) {
+        return $("<div/>").text(input).text();
+    }
+
+    // Username setzen
+    function setUsername() {
+        console.log('hier');
+
+        username = cleanInput(dialog.querySelector('.Username').value.trim()); //cleanInput($usernameInput.value().trim());
+        console.log(username);
+
+        // If the username is valid
+        if (username) {
+            $loginPage.fadeOut();
+            $chatPage.show();
+            $loginPage.off("click");
+            $currentInput = $inputMessage.focus();
+
+            // Tell the server your username
+            socket.emit("add user", username);
+        }
+    }
+
+    $usernameWeiter.on("click", function() {
+        setUsername();
+    });
+    
+    document.addEventListener("touchstart", enableNoSleep, false);
 });
