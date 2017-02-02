b8de0e77eadb29672be30cfe3ec32d185727fd1b
Jens Fischer <JensFZ@gmail.com>
2017-02-03 00:46:13 +0100

    some code cleanup

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.sockets.on("connection", function (socket) {
         console.log("user " + username + " connected");
     });
 
-socket.emit("chat", { zeit: new Date(), text: "Du bist nun mit dem Server verbunden!" });
+    socket.emit("chat", { zeit: new Date(), text: "Du bist nun mit dem Server verbunden!" });
     // wenn ein Benutzer einen Text senden
     socket.on("chat", function (data) {
         // so wird dieser Text an alle anderen Benutzer gesendet
